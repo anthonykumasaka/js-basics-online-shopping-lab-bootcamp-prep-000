d8a38ed456c0f765a66821021f4ed3f874a49da7
Anthony Kumasaka <anthonykumasaka@gmail.com>
2018-07-26 21:21:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -55,6 +55,6 @@ function placeOrder(cardNumber) {
  if (cardNumber === undefined) {
    return "Sorry, we don't have a credit card on file for you."
  } else {
-   return `Your total cost is $${totalPrice} + , which will be charged to the card ${cardNumber}.`; 
+   return `Your total cost is $${totalPrice}, which will be charged to the card ${cardNumber}.`; 
  }
 }
