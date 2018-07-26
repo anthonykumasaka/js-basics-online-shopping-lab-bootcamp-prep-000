bb12f03ca9a58b83e15b428d5ea3fc8288e58a66
Anthony Kumasaka <anthonykumasaka@gmail.com>
2018-07-26 20:56:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -43,10 +43,11 @@ function total() {
 }
 
 function removeFromCart(item) {
+  var newCart = []; 
   for (var i = 0; i < cart.length; i++) {
     if (cart[i].itemName === item){
-      cart.splice(i, 1); 
-    } return cart; 
+      newCart = cart.splice(i, 1); 
+    } return newCart; 
   }
 }
 
