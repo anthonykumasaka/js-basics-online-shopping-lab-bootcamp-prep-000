659281cd6c8e6f27576965a24729bd34c95e8a3f
Anthony Kumasaka <anthonykumasaka@gmail.com>
2018-07-26 20:53:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -43,7 +43,11 @@ function total() {
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for (var i = 0; i < cart.length; i++) {
+    if (cart[i].itemName === item){
+      
+    }
+  }
 }
 
 function placeOrder(cardNumber) {
