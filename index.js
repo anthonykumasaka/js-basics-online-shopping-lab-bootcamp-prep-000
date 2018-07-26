f1b328b3433ecbeb66c2701c04f387e9a025538d
Anthony Kumasaka <anthonykumasaka@gmail.com>
2018-07-26 19:54:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,12 +10,30 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var newObject = {}
+  newObject["itemName"] = item; 
+  newObject["itemPrice"] = Math.floor(Math.random() * 100);
+  cart.push(newObject); 
+  return `${item} has been added to your cart.`; 
 }
 
 function viewCart() {
-  // write your code here
+  var current = '';
+  if (cart.length === 0) {
+   return "Your shopping cart is empty." 
+  } else {
+   for (var i = 0; i < cart.length; i++){
+     if (i === cart.length - 1) {
+       current = current + `${cart[i].itemName} at $${cart[i].itemPrice}.`; 
+     } else if (i === 0) {
+       current = current + `${cart[i].itemName} at $${cart[i].itemPrice}, and `; 
+     } else {
+       current = current + `${cart[i].itemName} at $${cart[i].itemPrice}, `; 
+     }
+     }
+   } return "In your cart, you have " + current; 
 }
+
 
 function total() {
   // write your code here
