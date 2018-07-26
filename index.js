5cc99f69e4596723c4b4c102442dd1e93554dc30
Anthony Kumasaka <anthonykumasaka@gmail.com>
2018-07-26 21:09:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -46,10 +46,8 @@ function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++) {
     if (cart[i].itemName == item){
       return cart.splice(i, 1); 
-    } else {
-      return "That item is not in your cart."
-    }
-  } return "That item is not in your cart"
+    } 
+  } return "That item is not in your cart."
 }
 
 function placeOrder(cardNumber) {
