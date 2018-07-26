18a25d18fab98980cfa7caba4d7473373676b261
Anthony Kumasaka <anthonykumasaka@gmail.com>
2018-07-26 21:09:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function removeFromCart(item) {
     } else {
       return "That item is not in your cart."
     }
-  }
+  } return "That item is not in your cart"
 }
 
 function placeOrder(cardNumber) {
